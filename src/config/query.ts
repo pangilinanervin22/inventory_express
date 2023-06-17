70c3760931a81a3d6b4cfc11e89d3024d2c67006
Ervin Pangilinan <pangilinanervin22@gmail.com>
2023-06-17 23:58:44 +0800

    fix: query bug remove all database

--- src/config/query.ts
+++ src/config/query.ts
@@ -1,14 +1,13 @@
 export const instantiateDatabaseQueries = [
-	"DROP DATABASE IF EXISTS inventory;",
-	"CREATE DATABASE IF NOT EXISTS inventory;",
-	"USE inventory;",
-	`CREATE TABLE IF NOT EXISTS Product(
+        "CREATE DATABASE IF NOT EXISTS inventory;",
+        "USE inventory;",
+        `CREATE TABLE IF NOT EXISTS Product(
         product_id VARCHAR(255),
         name VARCHAR(70) NOT NULL,
         price FLOAT NOT NULL,
         brand VARCHAR(70) NOT NULL,
         PRIMARY KEY(product_id));`,
-	`
+        `
         CREATE TABLE IF NOT EXISTS Sales(
             sales_id VARCHAR(255) NOT NULL,
             product_id VARCHAR(255) NOT NULL, 
@@ -16,14 +15,14 @@ export const instantiateDatabaseQueries = [
             sales_date DATE DEFAULT(CURRENT_DATE), 
             PRIMARY KEY (sales_id),
             FOREIGN KEY(product_id) REFERENCES Product(product_id));`,
-	`CREATE TABLE IF NOT EXISTS Stock(
+        `CREATE TABLE IF NOT EXISTS Stock(
                 stock_id VARCHAR(255) NOT NULL,
                 product_id VARCHAR(255) NOT NULL,
                 quantity INT(100) NOT NULL,
                 production_date DATE DEFAULT(CURRENT_DATE), 
                 expiration_date DATE NOT NULL, 
                 PRIMARY KEY(stock_id));`,
-	`CREATE TABLE IF NOT EXISTS Employee (
+        `CREATE TABLE IF NOT EXISTS Employee (
                     employee_id VARCHAR(255),
                     name VARCHAR(70) NOT NULL,
                     username VARCHAR(70) NOT NULL,
@@ -32,11 +31,11 @@ export const instantiateDatabaseQueries = [
                     is_admin BOOLEAN DEFAULT(FALSE),
                     img_src VARCHAR(255) NOT NULL,
                     PRIMARY KEY(employee_id));`,
-	`
+        `
                     ALTER TABLE stock ADD CONSTRAINT product_stock 
                     FOREIGN KEY (product_id) REFERENCES employee(employee_id) 
                     ON DELETE CASCADE ON UPDATE RESTRICT; `,
-	`ALTER TABLE sales ADD CONSTRAINT product_sales FOREIGN KEY 
+        `ALTER TABLE sales ADD CONSTRAINT product_sales FOREIGN KEY 
                     (product_id) REFERENCES product(product_id) 
                     ON DELETE CASCADE ON UPDATE RESTRICT;`,
 ];
