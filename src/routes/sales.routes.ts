33e5090d2094618a70276796934ec43e6abff8d2
Ervin Pangilinan <pangilinanervin22@gmail.com>
2023-06-19 23:02:24 +0800

    add: sales route

--- src/routes/sales.routes.ts
+++ src/routes/sales.routes.ts
@@ -2,23 +2,23 @@ import express, { Request, Response } from "express";
 import sales from "../controller/sales";
 import { asyncHandle } from "../middleware/errorHandler";
 
-const routerStock = express.Router();
+const routerSales = express.Router();
 
 
-routerStock.post("/generate", asyncHandle(sales.generateSales));
-routerStock.get("/total", asyncHandle(sales.getAllSales));
+routerSales.post("/generate", asyncHandle(sales.generateSales));
+routerSales.get("/total", asyncHandle(sales.getAllSales));
 
 
-routerStock
+routerSales
     .route("/:id")
     .get(asyncHandle(sales.getSalesById))
     .delete(asyncHandle(sales.deleteSalesById))
     .put(asyncHandle(sales.updateSales));
 
 
-routerStock
+routerSales
     .route("/")
     .get(asyncHandle(sales.getAllSales))
     .post(asyncHandle(sales.createSales));
 
-export default routerStock;
+export default routerSales;
