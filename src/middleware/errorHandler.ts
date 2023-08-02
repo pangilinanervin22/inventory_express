e8be03253a864088f149f0333f1fbd0f2dd04599
Ervin <pangilinanervin22@gmail.com>
2023-08-02 23:48:18 +0800

    refactor: clean up and add some additional error message

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -10,38 +10,32 @@ export function asyncHandle(handler: Function) {
 	};
 }
 
-export const errorHandler = (
-	err: any,
-	req: Request,
-	res: Response,
-	next: NextFunction
-) => {
+export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
 	if (err.fatal) {
 		err.message = "Database is currently offline";
-		err.status = 501
+		err.status = 500;
 	}
 
-	const status = err.status || 400;
-	const message = getErrorMessage(err.message) || err;
-	console.log(err, "final");
+	const status = handleErrorMessage(err).status || 400;
+	const message = handleErrorMessage(err).message || err;
+	console.log(err, err.message, err.status);
 
 	res.status(status).send(message);
 };
 
-export const notFoundHandler = (
-	req: Request,
-	res: Response,
-	next: NextFunction
-) => {
+export const notFoundHandler = (req: Request, res: Response, next: NextFunction) => {
 	res.status(404).send("Route not found");
 };
 
-
-function getErrorMessage(message: string) {
-	switch (message) {
+function handleErrorMessage(error: { status: number; message: string }) {
+	switch (error.message) {
 		case "jwt malformed":
-			return "Invalid token";
+			return { status: 401, message: "Authorization error: token is corrupt" };
+		case "invalid signature":
+			return { status: 401, message: "Authorization error: token is invalid" };
+		case "unauthorized token":
+			return { status: 401, message: "Unauthorized token" };
 		default:
-			return message;
+			return error;
 	}
 }
